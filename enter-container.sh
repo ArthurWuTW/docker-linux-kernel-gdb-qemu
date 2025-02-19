32f00d57b7408792b47ab98105f062b78e109a5f
ArthurWuTW <lion811004@gmail.com>
2025-02-19 22:27:49 +0800

    Fix scripts using docker-compose v3

--- enter-container.sh
+++ enter-container.sh
@@ -1,4 +1,11 @@
-arr=(` docker-compose ps | awk 'NR>2 {split($0,arr," "); print arr[1]}' `)
+HOST_USER=$(whoami)
+PASSWD_USER_BY_SPACE=$(cat /etc/passwd | grep ${HOST_USER} | sed 's/:/ /g')
+PASSWD_USER_ARR=($PASSWD_USER_BY_SPACE)
+export HOSTUSER=${HOST_USER}
+export HOSTUID=${PASSWD_USER_ARR[2]}
+export HOSTGID=${PASSWD_USER_ARR[3]}
+
+arr=(` docker-compose ps | awk 'NR>1 {split($0,arr," "); print arr[1]}' `)
 echo "#######################################################"
 echo "Which container you want to enter?"
 for((i=0;i<${#arr[@]};i++));
--- status.sh
+++ status.sh
@@ -1,1 +1,8 @@
+HOST_USER=$(whoami)
+PASSWD_USER_BY_SPACE=$(cat /etc/passwd | grep ${HOST_USER} | sed 's/:/ /g')
+PASSWD_USER_ARR=($PASSWD_USER_BY_SPACE)
+export HOSTUSER=${HOST_USER}
+export HOSTUID=${PASSWD_USER_ARR[2]}
+export HOSTGID=${PASSWD_USER_ARR[3]}
+
 docker-compose ps
